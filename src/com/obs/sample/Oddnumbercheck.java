f89392dc6d286951f286e67deb2f0754a30ddf86
Aswathy-22 <138908063+Aswathy-22@users.noreply.github.com>
2023-07-26 20:42:44 +0530

    new changes

---
+++ src/com/obs/sample/Oddnumbercheck.java
@@ -0,0 +1,21 @@
+package com.obs.sample;
+
+import java.util.Scanner;
+
+public class Oddnumbercheck {
+    public static void main(String[] args)
+    {
+        int num;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        num = sc.nextInt();
+        if(num%2!=0)
+        {
+            System.out.println("Number is Odd");
+        }
+        else
+        {
+            System.out.println("Number is even");
+        }
+    }
+}
